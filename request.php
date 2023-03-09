df321d58dcc6fe28a99e12f5ed961ba97653f014
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-03-09 09:43:56 -0700

    Update request.php

--- request.php
+++ request.php
@@ -9,7 +9,7 @@ die ("Go back and try again.");
 $LINK_ADDR = "https://blockstream.info/testnet/address/$WALLET";
 echo "<h2>Check the transaction to the address requested:</h2>";
 echo "<b>Block explorer:</b><br><br>";
-echo '<a href="'.$LINK_ADDR.'">View wallet on block explorer</a>';
+echo '<a href="'.$LINK_ADDR.'">'.$WALLET.'</a>';
 ?>
 
 <?php
