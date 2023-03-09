18be5b1db3ec97043f496205f769980b58e68eb8
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-03-09 08:09:38 -0700

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -3,8 +3,8 @@ $WALLET = $_POST['WALLET_ADDR'];
 
 
 if(empty($WALLET )) {
-echo "<h2>You must enter an address to send the tokens to!</h2>\n" ;
-die ("Click Back to start again.");
+echo "<h2>You forgot to enter an address!!</h2>\n" ;
+die ("Go back and try again.");
 }
 echo "<h2>Check the transaction to the address requested:</h2>";
 echo "<b>Block explorer:</b><br><br>";
