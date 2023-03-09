8edf6ee691960a516d0e1dcc6b462fb42d71310e
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-03-09 00:05:41 -0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Request testnet BTC</title>
+</head>
+<link href="stylesheets/common.css" type="text/css" rel="stylesheet">
+
+<body>
+
+<h1>Enter Nomic-testnet-4d BTC deposit address</h1>
+
+<form method="post" action="send.php">
+
+Customer Name: <br />
+<input type="text" name="WALLET_ADDR" size="35" />
+<br />
+
+<input type="submit" value="Request tokens" />
+<br />
+</form>
+
+</body>
+</html>
