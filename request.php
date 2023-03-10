ce1fa637f1b87b3d606906c3dea3ab4416a8dbfd
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-03-10 13:03:37 -0700

    Update request.php

--- request.php
+++ request.php
@@ -6,12 +6,9 @@ if(empty($WALLET )) {
 echo "<h2>You forgot to enter an address!!</h2>\n" ;
 die ("Go back and try again.");
 }
+else
 $LINK_ADDR = "https://blockstream.info/testnet/address/$WALLET";
 echo "<h2>Check the transaction to the address requested:</h2>";
 echo "<b>Block explorer:</b><br><br>";
 echo '<a href="'.$LINK_ADDR.'">'.$WALLET.'</a>';
-?>
-
-<?php
-exec ( "/scripts/send.sh \"$WALLET\" );
 ?>
